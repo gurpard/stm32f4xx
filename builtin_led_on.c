e89b6fae060a460dd55d78e67366123dc2f8ca6c
gurpard <66367482+gurpard@users.noreply.github.com>
2024-11-08 17:49:34 +0700

    Update builtin_led_on.c

--- builtin_led_on.c
+++ builtin_led_on.c
@@ -6,7 +6,5 @@ int main(void)
 {
     RCC->AHB1ENR |= 1 << 0;          // Enable IO port A clock
     GPIOA->MODER |= 1 << (5 * 2);    // Turn on output mode for MODER5
-    GPIOA->PUPDR &= ~(3 << (5 * 2)); // Clear PUPDR5
-    GPIOA->PUPDR |= 1 << (5 * 2);    // Set pull-up mode for PA5 pin
     GPIOA->ODR |= 1 << 5;            // Set PA5 high to turn on LD2 LED
 }
